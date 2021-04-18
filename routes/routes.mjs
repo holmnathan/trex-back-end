88dfe495eaa9516bac0a86b9af05740300c4e019
Nathan Holm <nathan@wholm.com>
2021-04-17 23:39:26 -0700

    Add Event / Trip Routes to Routes Index

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -7,11 +7,13 @@ const router = Router(); // Return Express Router
 
 // Sub-Routes -----------------------------------------------------------------
 import userRoutes from './user.mjs'; // User Routes
-import tripRoutes from './trip.mjs'; // User Routes
+import tripRoutes from './trip.mjs'; // Trip Routes
+import eventRoutes from './event.mjs'; // Event Routes
 
 // Middleware -----------------------------------------------------------------
 router.use('/user', userRoutes);
 router.use('/trip', tripRoutes);
+router.use('/event', eventRoutes);
 
 // Routes ---------------------------------------------------------------------
 // GET ROUTE: "/"
